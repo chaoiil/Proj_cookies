b6d3d5dd0bfc9be5c42c5a397a7b3eb5baef8da1
Chaoiil <n.chaoil976@gmail.com>
2023-10-19 13:42:58 +0200

    initial commit

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,31 +1,70 @@
-let compter = 0;
+let compteur = 0;
+let multiplicateur = 1;
+let autoClickInterval;
+let rythmeClic = 0;
+let clic = 1;
+let prixB = 20;
+let prixD = 50;
+
 
 const Logoclick = document.getElementById('Logo_click');
 const span = document.getElementById('span_id');
 
+const LogoB = document.getElementById('LogoB');
+const spanB = document.getElementById('spanB');
+
+
+const autoclick = document.getElementById('clickauto')
+const span_auto = document.getElementById('span_id')
+
+const clickrythme = document.getElementById('clickrythme')
+const span_rythme = document.getElementById('span_rythme')
+
+
+// Le clicker 
 Logoclick.addEventListener("click", (event) => {
-  compter++;
-  span.innerHTML = compter;
+compteur += clic;
+span.innerHTML = compteur
+
 });
 
-const autoClick = setInterval(DebutAutoClick, Interval);
+LogoB.addEventListener("click", (event) => {
+if (compteur >= prixB) {
+compteur -= prixB;
+prixB += 20;
+compteur.innerHTML = compteur;
+spanB.innerHTML = prixB;
+clic++;
+} else {
+alert("Vous n'avez pas suffisamment de points pour acheter SUPER MARIO !");
+}
+});
 
 function DebutAutoClick() {
-  compter++;
-  span.innerHTML = compter;
+compteur++;
+span.innerHTML = compteur;
+;
+}
+LogoD.addEventListener("click", (event) => {
+ if (compteur >= 128000) {
+span.innerHTML = compteur;
+const autoClickInterval = setInterval(DebutAutoClick, 100);
+} 
+else {
+alert("Vous n'avez pas suffisamment de points pour acheter DIDIER DROGBA !");
 }
 
-function ArretAutoClick() {
-  clearInterval(autoClick);
-}
 
-document.addEventListener('Touche', (event) => {
-  if (event === 'v' || event === 'V') {
-    DebutAutoClick(); 
-  } else if (event === 'b' || event === 'B') {
-    ArretAutoClick(); 
-  }
+
+
+
+
 });
+
+
+
+
+
 
 
 
